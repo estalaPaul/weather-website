06627f180c6ff2732baad553960fd4828ddf31af
Paul Estala <32235423+estalaPaul@users.noreply.github.com>
2020-10-11 21:06:23 -0600

    feat: change to env

--- php/getWeather.php
+++ php/getWeather.php
@@ -2,12 +2,14 @@
     if (isset($_GET["city"]) && isset($_GET["unit"])) {
         $city = $_GET["city"];
         $unit = $_GET["unit"];
-        $config = parse_ini_file('routetoconfigfile');
+        $clientId = $_ENV["OPENWEATHER_API_KEY"];
         $ch = curl_init();
         
-        curl_setopt($ch, CURLOPT_URL, 'http://api.openweathermap.org/data/2.5/weather?q='.$city.'&units='.$unit.'&appid='.$config["clientId"]);
+        curl_setopt($ch, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q={$city}&units={$unit}&appid={$clientId}");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $weather = curl_exec($ch);
+
+        header('Content-Type: application/json');
         echo $weather;
     }
 ?>
