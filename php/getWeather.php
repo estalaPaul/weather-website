09912ac9334ccee5ad258a56075b11503b28ce7e
Paul Estala <32235423+estalaPaul@users.noreply.github.com>
2020-10-11 21:34:19 -0600

    fix: remove env

--- php/getWeather.php
+++ php/getWeather.php
@@ -2,10 +2,9 @@
     if (isset($_GET["city"]) && isset($_GET["unit"])) {
         $city = $_GET["city"];
         $unit = $_GET["unit"];
-        $clientId = $_ENV["OPENWEATHER_API_KEY"];
         $ch = curl_init();
         
-        curl_setopt($ch, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q={$city}&units={$unit}&appid={$clientId}");
+        curl_setopt($ch, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q={$city}&units={$unit}&appid=");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $weather = curl_exec($ch);
 
